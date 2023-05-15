7d5497b31f1a6e9f70eb3ec9a0c0ea10cc1f7243
cindy <1177031+cindywu@users.noreply.github.com>
2023-05-14 21:01:18 -1000

    add auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ClerkProvider } from '@clerk/nextjs'
 
-export default function App({ Component, pageProps }: AppProps ) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <ClerkProvider {...pageProps}>
+      <div className={'flex flex-row pt-4 justify-center items-center'}>
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
   )
 }
